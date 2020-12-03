ed01fdef544810b95ab71db50c1866b31f97467b
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-03 17:07:56 -0500

    Create solution.js

---
+++ 2015/day01/solution.js
@@ -0,0 +1,7 @@
+$('#answer span').text('Calculating...');
+$('#answer2 span').text('Calculating...');
+
+$.get( "input.txt", function( data ) {
+  $('#answer span').text( data.replace(/\(/g, "").length; - data.length );
+  //$('#answer2 span').text("");
+});
