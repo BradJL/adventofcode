5c6ed0df7e846b3a03378cacf512ba4eafc2fd67
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-03 17:25:40 -0500

    Update solution.js

--- 2015/day01/solution.js
+++ 2015/day01/solution.js
@@ -4,7 +4,7 @@ $('#answer2 span').text('Calculating...');
 $.get( "input.txt", function( data ) {
   let len = data.length - 1;
   let down = data.replace(/[^\)]/g, "").length;
-  let up = len - down.length;
+  let up = len - down;
   $('#answer span').text( "(Len:" +  len + ") Up " + up + ", Down " + down + " = " + (up - down) );
   //$('#answer2 span').text("");
 });
