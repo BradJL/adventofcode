cf271ef3ec24292c729262fb016ffe98ca458cd1
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-03 17:10:29 -0500

    Update solution.js

--- 2015/day01/solution.js
+++ 2015/day01/solution.js
@@ -2,6 +2,6 @@ $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
 $.get( "input.txt", function( data ) {
-  $('#answer span').text( data.replace(/\(/g, "").length; - data.length );
+  $('#answer span').text( data.replace(/[\(]/g, "").length; - data.length );
   //$('#answer2 span').text("");
 });
