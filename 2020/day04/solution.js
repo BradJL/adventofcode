8b63929b84cb1adf07e6301c0118d3921ab057f9
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-04 01:04:19 -0500

    Update solution.js

--- 2020/day04/solution.js
+++ 2020/day04/solution.js
@@ -14,14 +14,14 @@ function parseport(value, index, array) {
     
     // part 2
     items = value.replace(/cid:[0-9]*[ \n\t]/g,"");
-    let byr = items.match(/byr:[0-9]*[ \n\t]/g).trim().split(":");
-    let iyr = items.match(/iyr:[0-9]*[ \n\t]/g).trim().split(":");
-    let eyr = items.match(/eyr:[0-9]*[ \n\t]/g).trim().split(":");
-    let hgt = items.match(/hgt:[0-9]*(cm|in)[ \n\t]/g).trim().split(":");
+    let byr = items.match(/byr:[0-9]*[ \n\t]/g)[0].trim().split(":");
+    let iyr = items.match(/iyr:[0-9]*[ \n\t]/g)[0].trim().split(":");
+    let eyr = items.match(/eyr:[0-9]*[ \n\t]/g)[0].trim().split(":");
+    let hgt = items.match(/hgt:[0-9]*(cm|in)[ \n\t]/g)[0].trim().split(":");
     let hgtNum = hgt[1].replace(/[^0-9]/,"");
-    let hcl = items.match(/hcl:#[0-9a-f]{6}[ \n\t]/g).trim().split(":");
-    let ecl = items.match(/ecl:(amb|blu|brn|gry|grn|hxl|oth)[ \n\t]/g).trim().split(":");
-    let pid = items.match(/pid:[0-9]{9}[ \n\t]/g).trim().split(":");
+    let hcl = items.match(/hcl:#[0-9a-f]{6}[ \n\t]/g)[0].trim().split(":");
+    let ecl = items.match(/ecl:(amb|blu|brn|gry|grn|hxl|oth)[ \n\t]/g)[0].trim().split(":");
+    let pid = items.match(/pid:[0-9]{9}[ \n\t]/g)[0].trim().split(":");
     if( ( byr[1].length == 4 && 1920 <= byr[1] && byr[1] <= 2002 ) &&
         ( iyr[1].length == 4 && 2010 <= iyr[1] && iyr[1] <= 2020 ) &&
         ( eyr[1].length == 4 && 2020 <= eyr[1] && iyr[1] <= 2030 ) &&
