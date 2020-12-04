c98ab1749dc6674978b98eaea0b50e05cded558f
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-03 20:45:05 -0500

    Update solution.js

--- 2015/day02/solution.js
+++ 2015/day02/solution.js
@@ -4,8 +4,7 @@ $('#answer2 span').text('Calculating...');
 function calculateArea(value, index, array) {
   if( value == null || value == "" || value == undefined ) return;
 
-  let values = value.sort(function(a, b){return a-b}).split(/x/);
-  
+  let values = value.split(/x/);
   total += 3*values[0] + 2*values[1] + 2*values[2];
 }
 
@@ -13,7 +12,7 @@ var total = 0;
 
 $.get( "input.txt", function( data ) {
   // part 1
-  data.split(/\r?\n/).forEach(calculateArea);
+  data.sort(function(a, b){return a-b}).split(/\r?\n/).forEach(calculateArea);
   let len = data.length - 1;
   $('#input span').text( "(Bytes:" +  len + ")" );
   $('#answer span').text( total );
