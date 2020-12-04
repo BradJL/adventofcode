87f11627a6bf7eec8a727bf9443d901d55f55cd3
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-03 20:43:15 -0500

    Create solution.js

---
+++ 2015/day02/solution.js
@@ -0,0 +1,22 @@
+$('#answer span').text('Calculating...');
+$('#answer2 span').text('Calculating...');
+
+function calculateArea(value, index, array) {
+  if( value == null || value == "" || value == undefined ) return;
+
+  let values = value.sort(function(a, b){return a-b}).split(/x/);
+  
+  total += 3*values[0] + 2*values[1] + 2*values[2];
+}
+
+var total = 0;
+
+$.get( "input.txt", function( data ) {
+  // part 1
+  data.split(/\r?\n/).forEach(calculateArea);
+  let len = data.length - 1;
+  $('#input span').text( "(Bytes:" +  len + ")" );
+  $('#answer span').text( total );
+  
+  $('#answer2 span').text(i+1);
+});
