82fcf3c185c2716298028c6b6afeed2d10bf6711
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-03 21:04:03 -0500

    Update solution.js

--- 2015/day02/solution.js
+++ 2015/day02/solution.js
@@ -1,23 +1,30 @@
 $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
+var totalArea = 0;
+var linearFeet = 0;
+
 function calculateArea(value, index, array) {
   if( value == null || value == "" || value == undefined ) return;
 
+  // part 1
   let values = value.split(/x/).sort(function(a, b){return a-b});
   let areas = [ values[0] * values[1], values[1] * values[2], values[0] * values[2] ]
   //console.log( values );
-  total += (3*areas[0] + 2*areas[1] + 2*areas[2]);
+  totalArea += (3*areas[0] + 2*areas[1] + 2*areas[2]);
+  
+  //part 2
+  let volume = values[0] * values[1] * values[2];
+  let perimeter = 2* values[0] + 2 * values[1];
+  
+  linearFeet += (perimeter + volume);
 }
 
-var total = 0;
-
 $.get( "input.txt", function( data ) {
-  // part 1
   data.split(/\r?\n/).forEach(calculateArea);
   let len = data.length - 1;
   $('#input span').text( "(Bytes:" +  len + ")" );
-  $('#answer span').text( total );
+  $('#answer span').text( totalArea );
   
-  //$('#answer2 span').text();
+  $('#answer2 span').text( linearFeet );
 });
