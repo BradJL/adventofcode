234469de4b5d614bf85cb3ec349db4247681ff5a
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-05 01:21:23 -0500

    Update solution.js

--- 2020/day05/solution.js
+++ 2020/day05/solution.js
@@ -13,10 +13,12 @@ seats = '□'.repeat(128 * 8);
 
 var highestID = -1;
 
-function printSeats(){
+function seatingChart(){
+  let retval ="";
   for( let i = 0; i < 128; ++i ){
-    console.log( seats.substring(i*8,i*8+8) );
+    retval = retval.concat( seats.substring(i*8,i*8+8), "\n" );
   }
+  return retval;
 }
 function doItNTimes(value, index, array) {
   if( value == null || value == "" || value == undefined ) return; // May not be necessary anymore with the trim() call, below.
@@ -49,6 +51,6 @@ $.get( "input.txt", function( data ) {
   $('#input span').text('(Bytes: ' + (data.length) + ')');
   data.trim().split(/\r?\n/).forEach(doItNTimes);
   $('#answer span').text(highestID);
-  $('#answer2 span').text(seats.indexOf("■□■")+1);
+  $('#answer2 span').text( (seats.indexOf("■□■")+1 ) + "\n" + seatingChart() );
   printSeats();
 });
