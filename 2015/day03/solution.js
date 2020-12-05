d20244fe775147c7b76ec6d11a8c8cf7fcc2dcbd
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-05 12:47:00 -0500

    Update solution.js

--- 2015/day03/solution.js
+++ 2015/day03/solution.js
@@ -1,18 +1,33 @@
 $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
-function callback(value, index, array) {
-  if( value == null || value == "" || value == undefined ) return;
+var x = 0;
+var y = 0;
+var houses = new Set();
 
+function doItNTimes(value, index, array) {
+  //if( value == null || value == "" || value == undefined ) return;
   // part 1
+  switch( value ){
+  case '>':
+    ++x;
+  case '<':
+    --x;
+  case '^':
+    --y;
+  case 'v':
+    ++y;
+    break;
+  }
+  houses.add( x + "," + y );
   
-  //part 2
+  // part 2
 }
 
 $.get( "input.txt", function( data ) {
-  data.split(/\r?\n/).forEach(callback);
+  data.trim().split('').forEach(doItNTimes);
   $('#input span').text( "(Bytes:" +  len + ")" );
-  $('#answer span').text( totalArea );
+  $('#answer span').text( houses.length );
   
   $('#answer2 span').text( linearFeet );
 });
