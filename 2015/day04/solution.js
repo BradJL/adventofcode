8f1368d8790a2d7babcf571bef419cd333c21e79
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-05 13:42:26 -0500

    Update solution.js

--- 2015/day04/solution.js
+++ 2015/day04/solution.js
@@ -1,11 +1,8 @@
 $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
-function doItNTimes(value, index, array) {
-}
-
 $.get( "input.txt", function( data ) {
-  data.trim().split('').forEach(doItNTimes);
+  data.trim();
   $('#input span').text( "(Bytes:" +  data.length + ")" );
   //$('#answer span').text(  );  
   //$('#answer2 span').text(  );
