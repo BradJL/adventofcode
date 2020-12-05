6f78887be1e23841f39555b2af130ec1dbe67891
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-05 13:54:14 -0500

    Update solution.js

--- 2015/day04/solution.js
+++ 2015/day04/solution.js
@@ -2,15 +2,19 @@ $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
 $.get( "input.txt", function( data ) {
+  let ans = 1;
   let i = 1;
   let md5hash = md5(data.trim() + i);
   
-  while( md5hash.substring(0,5) != "00000" ){
+  while( md5hash.substring(0,5) != "000000" ){
     ++i;
     md5hash = md5(data.trim() + i);
+    if( md5hash.substring(0,5) != "00000" ){
+      ans = i;
+    }
   }
 
   $('#input span').text( "(Bytes:" +  data.length + ")" );
-  $('#answer span').text( i );
-  //$('#answer2 span').text(  );
+  $('#answer span').text( ans );
+  $('#answer2 span').text( i );
 });
