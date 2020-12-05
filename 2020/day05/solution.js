2ae44e706014e08b8b4fab0da665ea63094a6864
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-05 02:06:40 -0500

    Update solution.js

--- 2020/day05/solution.js
+++ 2020/day05/solution.js
@@ -15,31 +15,25 @@ function seatingChart(){
   let retval ="";
   for( let i = 0; i < 128; ++i ){
     let txt = (i*8).toString() + "-" + (i*8+8).toString() + " ";
-    //txt = "&nbsp;".repeat( 9 - txt.length ) + txt;
+    txt = "&nbsp;".repeat( 10 - txt.length ) + txt;
     
     retval = retval.concat( txt, seats.substring(i*8,i*8+8), newline );
   }
   return retval;
 }
+
 function doItNTimes(value, index, array) {
   if( value == null || value == "" || value == undefined ) return; // May not be necessary anymore with the trim() call, below.
   
   let row = value.replace(/[LR]/g, '').replace(/B/g, '1' ).replace(/F/g, '0' );
   let column = value.replace(/[BF]/g, '').replace(/R/g, '1' ).replace(/L/g, '0' );
   
-  //console.log( row + "," + column );
   row = parseInt( row, 2 );
   column = parseInt( column, 2 );
   
   let id = row * 8 + column;
   
   seats =seats.replaceAt(id, '■');
-  //console.log( row + "," + column + "," + id );
-  
-//  let thisID = 127;
-//  if( value[0] = 'F' ){ thisID /= 2; thisID.floor(); }
-//  FFBBFRRR
-  
   
   if( highestID < id ){
     highestID = id;
