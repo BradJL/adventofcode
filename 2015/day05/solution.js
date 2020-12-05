34437bc635ed3c809a0ec6545b45d881f05e95c9
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-05 14:32:09 -0500

    Update solution.js

--- 2015/day05/solution.js
+++ 2015/day05/solution.js
@@ -8,7 +8,7 @@ function doItNTimes(value, index, array) {
   let badStrings = value.match(/(ab|cd|pq|xy)/);
   let doubleLetters = value.match(/(aa|bb|cc|dd|ee|ff|gg|hh|ii|jj|kk|ll|mm|nn|oo|pp|qq|rr|ss|tt|uu|vv|ww|xx|yy|zz)/);
   
-  if( vowels.length >= 3 && badStrings != null && doubleLetters != null ){
+  if( vowels.length >= 3 && badStrings == null && doubleLetters != null ){
     ++niceStringsCount;
   }
 }
