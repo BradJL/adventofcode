83cdc69993268e57d4ec3c6ecbea01e2cf5a9eba
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-04 23:51:56 -0500

    Update solution.js

--- 2020/day05/solution.js
+++ 2020/day05/solution.js
@@ -1,1 +1,14 @@
+$('#answer span').text('Calculating...');
+$('#answer2 span').text('Calculating...');
 
+function doItNTimes(value, index, array) {
+  if( value == null || value == "" || value == undefined ) return; // May not be necessary anymore with the trim() call, below.
+
+}
+
+$.get( "input.txt", function( data ) {
+  $('#input span').text('(Bytes: ' + (data.length) + ')');
+  data.trim.split(/\r?\n/).forEach(doItNTimes);
+  $('#answer span').text("Answer 1");
+  $('#answer2 span').text("Answer 2");
+});
