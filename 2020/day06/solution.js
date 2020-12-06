fe98e7edfd687ba5d4e8cbc60098fb2a9dc4f436
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-06 00:23:47 -0500

    Update solution.js

--- 2020/day06/solution.js
+++ 2020/day06/solution.js
@@ -1,14 +1,17 @@
 $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
-function doItNTimes(value, index, array) {
+var sum = 0;
 
+function doItNTimes(value, index, array) {
+  let yesses = new Set( value.trim().replace(/[\r\n\t ]/g,"").split('') );
+  sum += yesses.size();
 }
 
 $.get( "input.txt", function( data ) {
   $('#input span').text('(Bytes: ' + (data.length) + ')');
-  data.trim().split(/\r?\n/).forEach(doItNTimes);
-  //$('#answer span').text(  );
+  data.trim().split(/\n\n/).forEach(doItNTimes);
+  $('#answer span').text( sum );
   //$('#answer2 span').text(  );
   //$('#bonus span').html(  );
 });
