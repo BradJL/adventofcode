d8c8cb8a7f21f17ab007a5a086ab7c1a71cc8074
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-06 00:58:52 -0500

    Update solution.js

--- 2020/day06/solution.js
+++ 2020/day06/solution.js
@@ -10,19 +10,33 @@ var sum = 0;
 var sum2 = 0;
 
 function doItNTimes(value, index, array) {
-  let yesses = new Set( value.trim().replace(/[\r\n\t ]/g,"").split('') );
-  sum += yesses.size;
+  let yesses = /*new Set(*/ value.trim().replace(/[\r\n\t ]/g,"").split('') //);
+  sum += (new Set( yesses )).size;
   
   let individuals = value.trim().split('\n');
-  
-  if( individuals.length == 1 ){ sum2 += individuals[0].length; return; }
-  
-  let groupYesses = individuals[0].split;
-  for( let i = 1; i < individuals.length; ++i ){
-    groupYesses = _.intersection( groupYesses, individuals[i] );
+//   let allYesses = new Set( value.trim().replace(/[\r\n\t ]/g,"").split('') );
+  yesses.sort();
+  let i = 0;
+  while( i < yesses.length ){
+    if( yesses[ i+individuals.length ] = yesses[i] ){
+      i += individuals.length;
+      ++sum2;
+    } else{ 
+      ++i;
+    }
   }
-  //let groupYesses = _.intersection( individuals );
-  sum2 += groupYesses.length;
+  
+  
+//   if( individuals.length == 1 ){ sum2 += individuals[0].length; return; }
+  
+//   let groupYesses = individuals[0].split;
+//   for( let i = 1; i < individuals.length; ++i ){
+//     groupYesses = _.intersection( groupYesses, individuals[i] );
+//   }
+//   //let groupYesses = _.intersection( individuals );
+//   sum2 += groupYesses.length;
+  
+  
 }
 
 $.get( "input.txt", function( data ) {
