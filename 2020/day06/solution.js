b12121f85f55fbe1fe2d65c7cf9e3ab15c4305d5
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-06 00:37:30 -0500

    Update solution.js

--- 2020/day06/solution.js
+++ 2020/day06/solution.js
@@ -2,10 +2,18 @@ $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
 var sum = 0;
+var sum2 = 0;
 
 function doItNTimes(value, index, array) {
   let yesses = new Set( value.trim().replace(/[\r\n\t ]/g,"").split('') );
   sum += yesses.size;
+  
+  let individuals = value.trim().split('\n');
+  let groupYesses = individuals[0].split;
+  for( let i = 1; i < individuals.length; ++i ){
+    groupYesses =  groupYesses.filter(value => individuals[i].includes(value));
+  }
+  sum2 = groupYesses.length;
 }
 
 $.get( "input.txt", function( data ) {
