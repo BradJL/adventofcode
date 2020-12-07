60fc673fff89c592d071efde4320d8a385a8ab4c
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-07 00:43:16 -0500

    Update solution.js

--- 2020/day07/solution.js
+++ 2020/day07/solution.js
@@ -19,7 +19,7 @@ $.get( "input.txt", function( data ) {
   input = data;
 });
 
-while( bagsArray.length() ){
+while( bagsArray.length ){
   input.match( new RegEx( ".*contain.*shiny gold bag", 'g' ) ).forEach(function(item) {
     add(item);
   });
