601b3e4192d5936a6ce8bf16cf5109306eb21a56
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-07 02:24:12 -0500

    Update solution.js

--- 2020/day07/solution.js
+++ 2020/day07/solution.js
@@ -1,10 +1,7 @@
 $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
-var bagsArray = [ "shiny gold bag" ];
-var bagsSet = new Set( bagsArray );
-
-function add( value ) {
+function add( value, bagsSet, bagsArray ) {
   let bag = value.replace(/s* contain.*/,"");
   let tmpSize = bagsSet.size;
   bagsSet.add( bag );
@@ -14,13 +11,15 @@ function add( value ) {
 }
 
 function outerBags( data ){
+  let bagsArray = [ "shiny gold bag" ];
+  let bagsSet = new Set( bagsArray );
   while( bagsArray.length ){
     let bag = bagsArray.pop();
     console.log( bag );
     let newBags = data.match( new RegExp( ".*contain.*" + bag, 'g' ) )
     if( newBags ){
       newBags.forEach(function(item) {
-        add(item);
+        add(item, bagsSet, bagsArray);
       });
     }
   let bonusTxt = "";
@@ -32,9 +31,11 @@ function outerBags( data ){
   $('#answer span').text( bagsSet.size - 1);
 }
 
+function innerBags( data ){
+}
+
 $.get( "input.txt", function( data ) {
   $('#input span').text('(Bytes: ' + (data.length) + ')');
   outerBags( data );
   //$('#answer2 span').text(  );
-  //bagsSet.sort();
 });
