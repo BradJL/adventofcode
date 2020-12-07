d76707cf1d11fb4ddaa2d7a095051fdcec91a9e7
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-07 00:38:16 -0500

    Update solution.js

--- 2020/day07/solution.js
+++ 2020/day07/solution.js
@@ -1,16 +1,29 @@
 $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
-function doItNTimes(value, index, array) {
+let bagArray = [ "shiny gold bag" ];
+let bagSet = new Set( bagArray );
+var input = "";
 
+function add( value ) {
+  value = value.replace(/s* contain/);
+  let tmpSize = bagSet.size;
+  bagSet.add( value );
+  if( bagSet.size > tmpSize ){
+    bagArray.push( value );
+  }
 }
 
 $.get( "input.txt", function( data ) {
   $('#input span').text('(Bytes: ' + (data.length) + ')');
-  //data.trim().split(/\r?\n/).forEach(doItNTimes);
-  let bags = data.match( /.*contain.*shiny gold bag/g );
-  
-  $('#answer span').text( bags.length );
-  //$('#answer2 span').text(  );
-  $('#bonus span').html( bags );
+  input = data;
 });
+
+while( bayArray.size() ){
+  input.match( new RegEx( ".*contain.*shiny gold bag", 'g' ).forEach(function(item) {
+    add(item);
+  });
+}
+$('#answer span').text( bagSet.size );
+//$('#answer2 span').text(  );
+$('#bonus span').html( bagSet );
