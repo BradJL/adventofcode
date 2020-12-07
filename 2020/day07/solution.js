16f7695c37f0925ae07c5dcfd90d8e5ec1a611d8
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-07 00:11:18 -0500

    Update solution.js

--- 2020/day07/solution.js
+++ 2020/day07/solution.js
@@ -7,8 +7,10 @@ function doItNTimes(value, index, array) {
 
 $.get( "input.txt", function( data ) {
   $('#input span').text('(Bytes: ' + (data.length) + ')');
-  data.trim().split(/\r?\n/).forEach(doItNTimes);
-  //$('#answer span').text(  );
+  //data.trim().split(/\r?\n/).forEach(doItNTimes);
+  let bags = data.match( /contain.*shiny gold bag/ );
+  
+  $('#answer span').text( bags.length );
   //$('#answer2 span').text(  );
   //$('#bonus span').html(  );
 });
