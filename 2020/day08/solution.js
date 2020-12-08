c041f3c78aabdbb6d80881166cc60e2933fed23c
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-08 00:22:24 -0500

    Update solution.js

--- 2020/day08/solution.js
+++ 2020/day08/solution.js
@@ -1,13 +1,31 @@
 $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
-function doItNTimes(value, index, array) {
-
+function part1(data) {
+  let acc = 0;
+  let line = 0;
+  let visitedLines = new Set();
+  
+  while( !visitedLines.has( line ) ){
+    visitedLines.add( line );
+    switch( data[line] ){
+    case acc:
+      acc += parseInt(data[line].replace(/[a-z +]/g,''));
+    case nop:
+      increment = 1;
+      break;
+    case jmp:
+      increment = parseInt(data[line].replace(/[a-z +]/g,''));
+      break;
+    }
+    line += increment;
+  }
+  $('#answer span').text( acc );
 }
 
 $.get( "input.txt", function( data ) {
   $('#input span').text('(Bytes: ' + (data.length) + ')');
-  data.trim().split(/\r?\n/).forEach(doItNTimes);
+  data.trim().split(/\r?\n/);//.forEach(doItNTimes);
   //$('#answer span').text(  );
   //$('#answer2 span').text(  );
   //$('#bonus span').html(  );
