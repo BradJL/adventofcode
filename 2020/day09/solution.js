1096183ff94513124230a73781172602eae950d7
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-09 00:12:17 -0500

    Update solution.js

--- 2020/day09/solution.js
+++ 2020/day09/solution.js
@@ -1,14 +1,31 @@
 $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
-function doItNTimes(value, index, array) {
-
+function part1() {
+  let curVal = 25;
+  
+  while( curVal < data.length ){
+    let i = curVal - 25
+    let j = curVal - 1;
+    while( i != j ){
+      if( input[i] + input[j] == curVal ){
+        ++curVal;
+        break;
+      } else if( input[i] + input[j] > 2020 ){
+        --j;
+      } else {
+        ++i;
+      }
+    }
+  }
+  $('#answer span').text( data[curVal] );
 }
 
 $.get( "input.txt", function( data ) {
   $('#input span').text('(Bytes: ' + (data.length) + ')');
-  data.trim().split(/\r?\n/).forEach(doItNTimes);
-  //$('#answer span').text(  );
+  data = data.trim().split(/\r?\n/);
+  
+  part1();
   //$('#answer2 span').text(  );
   //$('#bonus span').html(  );
 });
