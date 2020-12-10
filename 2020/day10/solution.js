4d5163e8b9b67820cef1d52ccafd93034a3083d5
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-10 00:19:13 -0500

    Update solution.js

--- 2020/day10/solution.js
+++ 2020/day10/solution.js
@@ -30,8 +30,12 @@ function part1( data ){
   //$('#bonus span').html(  );
 }
 function part2( data ){
-  data.forEach(function(value, index, array) {
-  });
+  let arrangemnet = 0;
+  let val = 0;
+  data.sort(function(a, b){return a-b});
+  console.log( data );
+//   data.forEach(function(value, index, array) {
+//   });
   //$('#answer2 span').text(  );
   //$('#bonus span').html(  );
 }
@@ -40,6 +44,6 @@ $.get( "input.txt", function( data ) {
   $('#input span').text('(Bytes: ' + (data.length) + ')');
   data = data.trim().split(/\r?\n/);
 //   data.forEach(doItNTimes);
-  part1( data );
-//   part2( data );
+  //part1( data );
+  part2( data );
 });
