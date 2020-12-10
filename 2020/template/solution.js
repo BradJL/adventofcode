a6aceee19bbc41172e2927a2510561797b139daf
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-09 23:41:11 -0500

    Update solution.js

--- 2020/template/solution.js
+++ 2020/template/solution.js
@@ -2,13 +2,24 @@ $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
 function doItNTimes(value, index, array) {
-
+}
+function part1( data ){
+  data.forEach(function(value, index, array) {
+  });
+  //$('#answer span').text(  );
+  //$('#bonus span').html(  );
+}
+function part2( data ){
+  data.forEach(function(value, index, array) {
+  });
+  //$('#answer2 span').text(  );
+  //$('#bonus span').html(  );
 }
 
 $.get( "input.txt", function( data ) {
   $('#input span').text('(Bytes: ' + (data.length) + ')');
-  data.trim().split(/\r?\n/).forEach(doItNTimes);
-  //$('#answer span').text(  );
-  //$('#answer2 span').text(  );
-  //$('#bonus span').html(  );
+  data = data.trim().split(/\r?\n/);
+//   data.forEach(doItNTimes);
+//   part1( data );
+//   part2( data );
 });
