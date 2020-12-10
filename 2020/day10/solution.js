92167eb5f81d62ed806a992aee53af68c8027887
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-10 00:07:46 -0500

    Update solution.js

--- 2020/day10/solution.js
+++ 2020/day10/solution.js
@@ -4,9 +4,26 @@ $('#answer2 span').text('Calculating...');
 function doItNTimes(value, index, array) {
 }
 function part1( data ){
-  data.forEach(function(value, index, array) {
-  });
-  //$('#answer span').text(  );
+  //data.forEach(function(value, index, array) {
+  //});
+  let val = 0;
+  data.sort(function(a, b){return a-b});
+  let one = 0;
+  let three = 0;
+  for( let i = 0; i < data.length; ++i ){
+    switch( data[i] ){
+      case 1:
+        ++one;
+        break;
+      case 3:
+        ++three;
+        break;
+      default:
+        break;
+    }
+    val = data[i];
+  }
+  $('#answer span').text( one * three );
   //$('#bonus span').html(  );
 }
 function part2( data ){
