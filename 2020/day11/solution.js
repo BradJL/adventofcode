94aa325f7ba3dbb4c74fd8c052833e1819afcb71
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-11 01:29:56 -0500

    Update solution.js

--- 2020/day11/solution.js
+++ 2020/day11/solution.js
@@ -8,7 +8,7 @@ function part1( data ){
   let newData = [];
   let changed = true;
   let openSeats = 0;
-//   while( changed ){
+  while( changed ){
     openSeats = 0;
     changed = false;
     newData = [];
@@ -51,7 +51,7 @@ function part1( data ){
     }
     console.log( newData );
     data = newData;
-//   }  
+  }  
   $('#answer span').text( openSeats );
   $('#bonus span').html( data );
 }
