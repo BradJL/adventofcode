9f4acf097b9bba5cce9ef556ba6764bdbd056dde
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-11 03:27:48 -0500

    Update solution.js

--- 2015/day05/solution.js
+++ 2015/day05/solution.js
@@ -2,8 +2,9 @@ $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
 var niceStringsCount = 0;
+var niceStringsCount2 = 0;
 
-function doItNTimes(value, index, array) {
+function part1(value, index, array) {
   let vowels = value.replace(/[^aeiou]/g,"");
   let badStrings = value.match(/(ab|cd|pq|xy)/);
   let doubleLetters = value.match(/(aa|bb|cc|dd|ee|ff|gg|hh|ii|jj|kk|ll|mm|nn|oo|pp|qq|rr|ss|tt|uu|vv|ww|xx|yy|zz)/);
@@ -16,14 +17,16 @@ function doItNTimes(value, index, array) {
 function part2( data ){
   let pairRepeat = data.match(/(..).*\1/);
   if( pairRepeat && pairRepeat[0].length > 3 ){ pairRepeat = true; };
-  return pairRepeat;
+  let charRepeatWOneCharBetwixt = data.match(/(.).*\1/);
+  if( charRepeatWOneCharBetwixt && charRepeatWOneCharBetwixt[0].length > 3 ){ charRepeatWOneCharBetwixt = true; };
+  return pairRepeat && charRepeatWOneCharBetwixt;
 }
 
 $.get( "input.txt", function( data ) {
   $('#input span').text('(Bytes: ' + (data.length) + ')');
-  data.trim().split(/\r?\n/).forEach(doItNTimes);
+  data.trim().split(/\r?\n/).forEach(part1);
   $('#answer span').text( niceStringsCount );
   
-  $('#answer2 span').text( part2( "abcdefcdghij" ) );
+  $('#answer2 span').text( part2( "qjhvhtzxzqqjkmpb" ) );
   //$('#bonus span').html(  );
 });
