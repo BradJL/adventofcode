5235c8c31b0ea61bb5c183abdc36b6af96d7a4b2
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-11 04:07:44 -0500

    Update solution.js

--- 2015/day05/solution.js
+++ 2015/day05/solution.js
@@ -6,8 +6,9 @@ function part1(data) {
   data.forEach(function(value, index, array) {
     let vowels = value.replace(/[^aeiou]/g,"");
     let badStrings = value.match(/(ab|cd|pq|xy)/);
-//     let doubleLetters = value.match(/(aa|bb|cc|dd|ee|ff|gg|hh|ii|jj|kk|ll|mm|nn|oo|pp|qq|rr|ss|tt|uu|vv|ww|xx|yy|zz)/);
-    let doubleLetters = value.match(/(.)\1/);
+    let doubleLetters
+      // = value.match(/(aa|bb|cc|dd|ee|ff|gg|hh|ii|jj|kk|ll|mm|nn|oo|pp|qq|rr|ss|tt|uu|vv|ww|xx|yy|zz)/);
+      = value.match(/(.)\1/);
 
     if( vowels.length >= 3 && badStrings == null && doubleLetters != null ){
       ++niceStringsCount;
