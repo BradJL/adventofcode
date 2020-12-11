4d07c2d04c20f319403e4e421d0f23e5bdace3c9
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-11 00:20:39 -0500

    Update solution.js

--- 2020/day11/solution.js
+++ 2020/day11/solution.js
@@ -2,9 +2,27 @@ $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
 function part1( data ){
-  data.forEach(function(value, index, array) {
-  });
-  //$('#answer span').text(  );
+  //data.forEach(function(value, index, array) {
+  //});
+  
+  for( let i = 0; i < data.length; ++i ){
+    for( let j = 0; j < data[i].length; ++j ){
+      let count = 0;
+      if( i > 0 && j > 0 && data[i-1].charAt(j - 1) == "#" ) ++count;
+      if( i > 0 && data[i-1].charAt(j) == "#" ) ++count;
+      if( i > 0 && j < data.length - 1 && data[i-1].charAt(j + 1) == "#" ) ++count;
+      if( j > 0 && data[i].charAt(j - 1) == "#" ) ++count;
+      if( j < data.length - 1 && data[i].charAt(j + 1) == "#" ) ++count;
+      if( i < data.length - 1 && j > 0 && data[i+1].charAt(j - 1) == "#" ) ++count;
+      if( i > 0 && data[i+1].charAt(j) == "#" ) ++count;
+      if( i < data.length - 1 && j < data.length - 1 && data[i+1].charAt(j + 1) == "#" ) ++count;
+      if( count >= 1 ){
+        data[i] = data[i].substring(0,i) + '#' + data[i].substring(i+1);
+      }
+    }
+  }
+  
+  $('#answer span').text( data );
   //$('#bonus span').html(  );
 }
 function part2( data ){
@@ -17,13 +35,13 @@ function part2( data ){
 $.get( "input.txt", function( data ) {
   $('#input span').text('(Bytes: ' + (data.length) + ')');
   data = data.trim().split(/\r?\n/);
-//   part1( data );
+  part1( data );
 //   part2( data );
   
-  let input = [];
-  data.forEach(function(number, index, array) {
-    input.push(parseInt(number))
-  });
-  part1( input );
-  part2( input );
+//   let input = [];
+//   data.forEach(function(number, index, array) {
+//     input.push(parseInt(number))
+//   });
+//   part1( input );
+//   part2( input );
 });
