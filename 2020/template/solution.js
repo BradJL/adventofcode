e1e42a614b7f711288a584ea9a0fcc1052109ff2
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-11 23:08:20 -0500

    Update solution.js

--- 2020/template/solution.js
+++ 2020/template/solution.js
@@ -1,6 +1,11 @@
 $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
+// var canvas = document.getElementById("visualization");
+// var ctx = canvas.getContext("2d");
+// var canvas2 = document.getElementById("visualization2");
+// var ctx2 = canvas2.getContext("2d");
+
 function part1( data ){
   data.forEach(function(value, index, array) {
   });
