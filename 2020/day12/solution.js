ed18f4eb1f2be1c644edd0ab62c03018c6fd4ffb
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-12 00:15:05 -0500

    Update solution.js

--- 2020/day12/solution.js
+++ 2020/day12/solution.js
@@ -1,4 +1,3 @@
-  
 $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
@@ -8,9 +7,79 @@ $('#answer2 span').text('Calculating...');
 // var ctx2 = canvas2.getContext("2d");
 
 function part1( data ){
+  let direction = 'E';
+  let degrees = 0;
+  let n = 0;
+  let e = 0;
   data.forEach(function(value, index, array) {
+    let inst = value.charAt(0);
+    let num = parseInt(value.substring(1));
+    switch( inst ){
+    case 'N':
+      n += num;
+      break;
+    case 'S':
+      n -= num;
+      break;
+    case 'E':
+      e += num;
+      break;
+    case 'W':
+      e -= num;
+      break;
+    case 'L':
+      degrees = (degrees - num) % 360;
+      switch( degrees ){
+      case 0:
+        direction = 'E';
+        break;
+      case 90:
+        direction = 'S';
+        break;
+      case 180:
+        direction = 'W';
+        break;
+      case 270:
+        direction = 'N';
+        break;
+      }
+      break;
+    case 'R':
+      degrees = (degrees + num) % 360;
+      switch( degrees ){
+      case 0:
+        direction = 'E';
+        break;
+      case 90:
+        direction = 'S';
+        break;
+      case 180:
+        direction = 'W';
+        break;
+      case 270:
+        direction = 'N';
+        break;
+      }
+      break;
+    case 'F':
+      switch( direction ){
+      case 'N':
+        n += num;
+        break;
+      case 'S':
+        n -= num;
+        break;
+      case 'E':
+        e += num;
+        break;
+      case 'W':
+        e -= num;
+        break;
+      }
+      break;
+    }
   });
-  //$('#answer span').text(  );
+  $('#answer span').text( n + e );
   //$('#bonus span').html(  );
 }
 function part2( data ){
@@ -24,7 +93,7 @@ $.get( "input.txt", function( data ) {
 //   $('#input span').text('(Bytes: ' + (data.length) + ')');
   data = data.trim().split(/\r?\n/);
   $('#input span').text('(Lines: ' + (data.length) + ')');
-//   part1( data );
+  part1( data );
 //   part2( data );
   
 //   let input = [];
