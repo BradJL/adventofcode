c54c06c3ecc6bbd06f21a61f93a33c3ba450114a
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-12 01:16:45 -0500

    Update solution.js

--- 2020/day12/solution.js
+++ 2020/day12/solution.js
@@ -3,8 +3,8 @@ $('#answer2 span').text('Calculating...');
 
 var canvas = document.getElementById("visualization");
 var ctx = canvas.getContext("2d");
-// var canvas2 = document.getElementById("visualization2");
-// var ctx2 = canvas2.getContext("2d");
+var canvas2 = document.getElementById("visualization2");
+var ctx2 = canvas2.getContext("2d");
 
 function part1( data ){
   ctx.strokeStyle = "#FF0000";
@@ -101,9 +101,13 @@ function part1( data ){
 function part2( data ){
   let wE = 10;
   let wN = 1;
+  let prevWN = 10;
+  let prevWE = 1;
   let degrees = 0;
   let n = 0;
   let e = 0;
+  let prevN = 0;
+  let prevE = 0;
   let oldWN;
   data.forEach(function(value, index, array) {
     let inst = value.charAt(0);
@@ -173,6 +177,13 @@ function part2( data ){
       console.log( "F" + num + " N" + n + "E" + e );
       break;
     }
+    ctx2.strokeStyle = "#FF0000";
+    ctx2.beginPath();
+    ctx2.moveTo(500 - prevE, 500 - prevN);
+    ctx2.lineTo(500 - e, 500 - n);
+    ctx2.stroke();
+    prevN = n;
+    prevE = e;
   });
   $('#answer2 span').text( Math.abs(n) + Math.abs(e) );
   //$('#bonus span').html(  );
@@ -184,11 +195,4 @@ $.get( "input.txt", function( data ) {
   $('#input span').text('(Lines: ' + (data.length) + ')');
   part1( data );
   part2( data );
-  
-//   let input = [];
-//   data.forEach(function(number, index, array) {
-//     input.push(parseInt(number))
-//   });
-//   part1( input );
-//   part2( input );
 });
