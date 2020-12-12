6dbc588f951f8eb5a68e42234778f0d20fb8e8a4
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-11 20:34:17 -0500

    Update solution.js

--- 2015/day06/solution.js
+++ 2015/day06/solution.js
@@ -3,6 +3,8 @@ $('#answer2 span').text('Calculating...');
 
 var canvas = document.getElementById("visualization");
 var ctx = canvas.getContext("2d");
+var canvas2 = document.getElementById("visualization2");
+var ctx2 = canvas2.getContext("2d");
 
 function makeArray(w, h, val) {
     var arr = [];
@@ -16,12 +18,14 @@ function makeArray(w, h, val) {
 } // thanks, internet!
 
 function part1( data ){
-  let drawIt = false;
+  let drawIt = true;
   let lights = makeArray( 1000, 1000, false );
   let lights2 = makeArray( 1000, 1000, 0 );
   if( drawIt ){
     ctx.fillStyle = "#000000";
     ctx.fillRect(0,0,999,999);
+    ctx2.fillStyle = "#000000";
+    ctx2.fillRect(0,0,999,999);
   }
 
   let lit = 0;
@@ -52,6 +56,9 @@ function part1( data ){
           }
           ++(lights2[x][y]);
           ++brightness;
+          ctx2.fillStyle = "rgb(" + (3 + 6 * lights2[x][y]) + ", " + (3 + 6 * lights2[x][y]) + ", "+ (3 + 6 * lights2[x][y]) + ")";
+          ctx2.fillRect(x, y, 1, 1);
+
           if( lights2[x][y] > maxBrightness ){
             maxBrightness = lights2[x][y];
           }
@@ -75,6 +82,8 @@ function part1( data ){
           } else {
             --brightness;
           }
+          ctx2.fillStyle = "rgb(" + (3 + 6 * lights2[x][y]) + ", " + (3 + 6 * lights2[x][y]) + ", "+ (3 + 6 * lights2[x][y]) + ")";
+          ctx2.fillRect(x, y, 1, 1);
         }
       }
       break;
@@ -98,6 +107,9 @@ function part1( data ){
           }
           lights2[x][y] += 2;
           brightness += 2;
+          ctx2.fillStyle = "rgb(" + (3 + 6 * lights2[x][y]) + ", " + (3 + 6 * lights2[x][y]) + ", "+ (3 + 6 * lights2[x][y]) + ")";
+          ctx2.fillRect(x, y, 1, 1);
+
           if( lights2[x][y] > maxBrightness ){
             maxBrightness = lights2[x][y];
           }
@@ -108,7 +120,7 @@ function part1( data ){
   });
   $('#answer span').text( lit );
   $('#answer2 span').text( brightness );
-  $('#bonus span').html( maxBrightness );
+  //$('#bonus span').html( maxBrightness );
 }
 
 $.get( "input.txt", function( data ) {;
