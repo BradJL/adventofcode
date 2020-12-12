977571bcf7a482322b5d4df107dc20dc87c6be13
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-11 23:09:59 -0500

    Create solution.js

---
+++ 2020/day12/solution.js
@@ -0,0 +1,36 @@
+  
+$('#answer span').text('Calculating...');
+$('#answer2 span').text('Calculating...');
+
+// var canvas = document.getElementById("visualization");
+// var ctx = canvas.getContext("2d");
+// var canvas2 = document.getElementById("visualization2");
+// var ctx2 = canvas2.getContext("2d");
+
+function part1( data ){
+  data.forEach(function(value, index, array) {
+  });
+  //$('#answer span').text(  );
+  //$('#bonus span').html(  );
+}
+function part2( data ){
+  data.forEach(function(value, index, array) {
+  });
+  //$('#answer2 span').text(  );
+  //$('#bonus span').html(  );
+}
+
+$.get( "input.txt", function( data ) {
+//   $('#input span').text('(Bytes: ' + (data.length) + ')');
+  data = data.trim().split(/\r?\n/);
+  $('#input span').text('(Lines: ' + (data.length) + ')');
+//   part1( data );
+//   part2( data );
+  
+//   let input = [];
+//   data.forEach(function(number, index, array) {
+//     input.push(parseInt(number))
+//   });
+//   part1( input );
+//   part2( input );
+});
