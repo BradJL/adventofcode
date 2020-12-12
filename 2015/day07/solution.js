5250bf1b350bcc1c685a5d294ab71219cc8edf29
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-12 02:35:19 -0500

    Update solution.js

--- 2015/day07/solution.js
+++ 2015/day07/solution.js
@@ -32,7 +32,7 @@ $.get( "input.txt", function( data ) {
 //   data = data.trim().split(/\r?\n/);
   let circuit = 'a';
   let wiresSet = new Set(getWires( circuit ));
-  let a = Array.from(a, e => getInputs( e, data ));
+  let a = Array.from(wiresSet, e => getInputs( e, data ));
   
   $('#answer span').text( a );
 //   $('#input span').text('(Lines: ' + (data.length) + ')');
