1b89375e1cdd10f35277b75effe918628cc4ab73
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-13 16:16:42 -0500

    Update solution.js

--- 2020/day13/solution.js
+++ 2020/day13/solution.js
@@ -77,7 +77,8 @@ $.get( "input.txt", function( data ) {
   //part2( "67,x,7,59,61" );
 //   part2( data[1] );
 //   part2( "17,x,x,x,x,x,x,41" ); //34
-  part2( "17,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,523" );
+//   part2( "17,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,523" ); //106675
+  part2( "17,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,523,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13" );
 //   part2( "17,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,523,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,19,x,x,x,23,x,x,x,x,x,x,x,787,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29" );
 //   let input = [];
 //   data.forEach(function(number, index, array) {
