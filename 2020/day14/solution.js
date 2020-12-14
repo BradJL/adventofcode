51159c61f2465e2430b474b83c5e25299d16c8e1
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-14 00:22:26 -0500

    Update solution.js

--- 2020/day14/solution.js
+++ 2020/day14/solution.js
@@ -6,8 +6,30 @@ $('#answer2 span').text('Calculating...');
 // var canvas2 = document.getElementById("visualization2");
 // var ctx2 = canvas2.getContext("2d");
 
+function applyMask( mask, number ){
+  number = number.toString(2);
+  for( let i = 0; i < mask.length; ++i ){
+    if( mask[i] == 0 ){
+      number = number.substring(0, i); + '0' + number.substring(i);
+    } else if( mask[i] == 1 ){
+      number = number.substring(0, i); + '1' + number.substring(i);
+    }
+  }
+  return( parseInt(number, 2) );
+}
+
 function part1( data ){
+  let mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+  let memory = [];
   data.forEach(function(value, index, array) {
+    let leftRight = value.split(/ = /);
+    if( leftRight[0] == "mask" ){
+      mask = leftRight[1];
+    } else {
+      let numbers = value.match(/[0-9]+/g);
+      memory[parseInt( numbers[0] )] = applyMask( mask, parseInt( numbers[1] ) );
+      console.log( memory[parseInt( numbers[0] )];
+    }
   });
   //$('#answer span').text(  );
   //$('#bonus span').html(  );
@@ -22,9 +44,10 @@ function part2( data ){
 $.get( "input.txt", function( data ) {
 //   $('#input span').text('(Bytes: ' + (data.length) + ')');
   data = data.trim().split(/\r?\n/);
+  data = "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X\nmem[8] = 11\nmem[7] = 101\nmem[8] = 0";
   $('#input span').text('(Lines: ' + (data.length) + ')');
-//   part1( data );
-//   part2( data );
+  part1( data );
+  part2( data );
   
 //   let input = [];
 //   data.forEach(function(number, index, array) {
