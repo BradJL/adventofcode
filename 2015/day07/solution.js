d71d2d392b4306e61cf7334477a3400e9794d197
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-14 23:53:46 -0500

    Update solution.js

--- 2015/day07/solution.js
+++ 2015/day07/solution.js
@@ -18,7 +18,7 @@ function recurse( wire, circuit ){
   }
   if( circuit[wire].match(/AND/) ){
     let wires = circuit[wire].match(/[a-z0-9]+/g);
-    console.log( wires[0] + " AND " + wires[1] );
+    console.log( "AND " + wires[0] + "," + wires[1] );
     return ( recurse(wires[0], circuit) & recurse(wires[1], circuit) );
   }
   if( circuit[wire].match(/OR/) ){
