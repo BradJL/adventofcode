48384b380863f914bed718738b0199b64f3baaab
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-15 00:39:12 -0500

    Update solution.js

--- 2020/day15/solution.js
+++ 2020/day15/solution.js
@@ -35,10 +35,33 @@ function part1( data, iterations ){
   $('#answer span').text( lastNum );
   //$('#bonus span').html(  );
 }
-function part2( data ){
-  data.forEach(function(value, index, array) {
+function part2( data, iterations ){
+  let turn = 0
+  let lastNum;
+  let lastSpoken = 0;
+  let numbers = [];
+  data.split(/,/).forEach(function(value, index, array) {
+    ++turn;
+    lastNum = parseInt(value);
+    lastSpoken = numbers[value];
+    numbers[ lastNum ] = turn; console.log( "adding " + lastNum + "(" + turn + ")" );
   });
-  //$('#answer2 span').text(  );
+  
+  do {
+    if(lastSpoken) { // if it's been said before
+      //console.log( "found " + lastNum );
+      // The == and != operators consider null equal to only null or undefined
+      lastNum = turn - lastSpoken;
+    } else {
+      //console.log( "didn't find " + lastNum );
+      lastNum = 0;
+    }
+    ++turn;
+    console.log( "saying " + lastNum );
+    lastSpoken = numbers[lastNum];
+    numbers[ lastNum ] = turn; console.log( "adding " + lastNum + "(" + turn + ")" );
+  } while( turn < iterations );
+  $('#answer2 span').text( lastNum );
   //$('#bonus span').html(  );
 }
 
@@ -49,6 +72,7 @@ $.get( "input.txt", function( data ) {
 //   part1( "0,3,6", 10 );
 //   part1( "1,3,2", 2020 );
   part1( data, 2020 );
+  part2( data, 30000000 );
 //   part2( data );
   
 //   let input = [];
