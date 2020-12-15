bc17273f3d18c36c6eb9cbb764815f944f2bace3
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-14 23:29:15 -0500

    Update solution.js

--- 2015/day07/solution.js
+++ 2015/day07/solution.js
@@ -7,16 +7,49 @@ $('#answer2 span').text('Calculating...');
 // var ctx2 = canvas2.getContext("2d");
 
 function recurse( wire, circuit ){
-  return circuit[wire];
+  console.log( circuit[wire] );
+  if( circuit[wire].match(/RSHIFT/) ){
+    console.log( "RSHIFT" );
+    return recurse( (circuit[wire].match(/[a-z]+/)) >> parseInt(circuit[wire].match(/[0-9]+/)) );
+  }
+  if( circuit[wire].match(/LSHIFT/) ){
+    console.log( "LSHIFT" );
+    return recurse( (circuit[wvalue.match(/[a-z]+/)) << parseInt(circuit[wire].match(/[0-9]+/)) );
+  }
+  if( circuit[wire].match(/AND/) ){
+    console.log( "AND" );
+    let wires = circuit[wire].match(/[a-z0-9]+/g);
+    return recurse( (wires[0]) & (wires[1]) );
+  }
+  if( circuit[wire].match(/OR/) ){
+    console.log( "OR" );
+    let wires = circuit[wire].match(/[a-z0-9]+/g);
+    return recurse( (wires[0]) | (wires[1]) );
+  }
+  if( circuit[wire].match(/NOT/) ){
+    console.log( "NOT" );
+    let wires = circuit[wire].match(/[0-9]+/);
+    return( ~(recurse( wires[0])) );
+  }
+  if( circuit[wire].match(/[a-z]+/) ){
+    console.log( "wire" );
+    let wires = circuit[wire].match(/[a-z]+/);
+    return( recurse( wires[0]) );
+  }
+  if( circuit[wire].match(/[0-9]+/) ){
+    console.log( "value" );
+    return parseInt(wire);  // expected to only be numbers at this point.
+  }
+  return ;
 }
 
-function part1( data ){
+function part1( data, wire ){
   let circuit = [];
   data.forEach(function(value, index, array) {
     let leftAndRight = value.split(/ -> /);
     circuit[leftAndRight[1]] = leftAndRight[0];
   });
-  $('#answer span').text( recurse('a', circuit) );
+  $('#answer span').text( recurse(wire, circuit) );
   //$('#bonus span').html(  );
 }
 function part2( data ){
@@ -37,7 +70,15 @@ function getWires( circuit ){
 $.get( "input.txt", function( data ) {
   $('#input span').text('(Bytes: ' + (data.length) + ')');
   data = data.trim().split(/\r?\n/);
-  part1( data );
+  data = [ "123 -> x",
+            "456 -> y",
+            "x AND y -> d",
+            "x OR y -> e",
+            "x LSHIFT 2 -> f",
+            "y RSHIFT 2 -> g",
+            "NOT x -> h",
+            "NOT y -> i" ];
+  part1( data, 'd' );
 //   let circuit = '( lx )';
 //   let wiresSet = new Set(getWires( circuit ));
 //   while( wiresSet.size > 0 ){
