8548cfedc245b719871ebbe6319281d849a4795a
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-15 15:49:30 -0500

    Update solution.js

--- 2015/day07/solution.js
+++ 2015/day07/solution.js
@@ -92,8 +92,8 @@ $.get( "input.txt", function( data ) {
    $('#bonus span').html(
      "f: " + part1( data, 'f' ) + "<br />" +
      "g: " + part1( data, 'g' ) + "<br />" +
-     "h: " + part1( data, 'h' ) + "<br />" +
-     "i: " + part1( data, 'i' ) + "<br />" +
+//      "h: " + part1( data, 'h' ) + "<br />" +
+//      "i: " + part1( data, 'i' ) + "<br />" +
      "x: " + part1( data, 'x' ) + "<br />" +
      "y: " + part1( data, 'y' )
                         );
