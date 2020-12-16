e8ff897ccc0fb0cfab39f4349e9837769000a8dd
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-15 23:37:38 -0500

    Update solution.js

--- 2020/day16/solution.js
+++ 2020/day16/solution.js
@@ -1,14 +1,26 @@
-// function initialize(){
-//   $('#answer span').text('Calculating...');
-//   $('#answer2 span').text('Calculating...');
+function initialize(){
+  $('#answer span').text('Calculating...');
+  $('#answer2 span').text('Calculating...');
   
-//   // var canvas = document.getElementById("visualization");
-//   // var ctx = canvas.getContext("2d");
-//   // var canvas2 = document.getElementById("visualization2");
-//   // var ctx2 = canvas2.getContext("2d");  
-// }
+  // var canvas = document.getElementById("visualization");
+  // var ctx = canvas.getContext("2d");
+  // var canvas2 = document.getElementById("visualization2");
+  // var ctx2 = canvas2.getContext("2d");  
+}
 
-const fs = require('fs').promises;
+// const fs = require('fs').promises;
+// async function readFile(filePath){
+//   try {
+//     const constData = await fs.readFile(filePath);
+//     let part1Answer = part1( constData, 'a' );
+//     console.log( "Part 1 answer: " + part1Answer );
+
+//     let part2Answer = part2( constData, 'a', part1Answer );
+//     console.log( "Part 2 answer: " + part2Answer );
+//   } catch (error) {
+//     console.error(`Got an error trying to read the file: ${error.message}`);
+//   }
+// }
 
 function log( what ){
   //console.log( what );
@@ -23,19 +35,6 @@ function part2( data, wire, part1Answer ){
   data = data.toString().trim().split(/\r?\n/);
 
   return 0;
-}
-
-async function readFile(filePath){
-  try {
-    const constData = await fs.readFile(filePath);
-    let part1Answer = part1( constData, 'a' );
-    console.log( "Part 1 answer: " + part1Answer );
-
-    let part2Answer = part2( constData, 'a', part1Answer );
-    console.log( "Part 2 answer: " + part2Answer );
-  } catch (error) {
-    console.error(`Got an error trying to read the file: ${error.message}`);
-  }
 }
 
 function readFile(filePath){
