55e9f2cac194c5f1877860e3cc2105cc95988508
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-15 21:13:38 -0500

    Update solution.js

--- 2015/day08/solution.js
+++ 2015/day08/solution.js
@@ -17,7 +17,7 @@ function part2( data ){
   data = data.trim().split(/\r?\n/);
   let stringChars = 0;
   data.forEach(function(line, index, array) {
-    let chars += line.replace(/\\/g,"\\").replace(/"/,'\"');
+    let chars = line.replace(/\\/g,"\\").replace(/"/,'\"');
     console.log( chars.length + ": " + chars.length );
     stringChars += chars.length;
   });
