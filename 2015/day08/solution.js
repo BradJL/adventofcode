f54bfb431063ad07b46174930a8b63a2dd409a57
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-15 21:19:30 -0500

    Update solution.js

--- 2015/day08/solution.js
+++ 2015/day08/solution.js
@@ -1,39 +1,25 @@
 $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
-// var canvas = document.getElementById("visualization");
-// var ctx = canvas.getContext("2d");
-// var canvas2 = document.getElementById("visualization2");
-// var ctx2 = canvas2.getContext("2d");
-
 function part1( data ){
   let codeChars = data.replace(/\r?\n/g,'').length;
   let stringChars = data.trim().replace(/\\["\\]/g,'_').replace(/\\x[0-9a-fA-F][0-9a-fA-F]/g,'_').replace(/"\r?\n"/g,'').replace(/^"/,'').replace(/"$/,'');
   $('#answer span').text( codeChars + " - " + stringChars.length + " = " + (codeChars - stringChars.length) );
-//   $('#bonus span').text( stringChars );
 }
 function part2( data ){
   let codeChars = data.replace(/\r?\n/g,'').length;
   data = data.trim().split(/\r?\n/);
   let stringChars = 0;
   data.forEach(function(line, index, array) {
-    let chars = line.replace(/\\/g,"__").replace(/"/g,'__');
+    let chars = line.replace(/\\/g,"\\").replace(/"/g,'\"');
     console.log( (chars.length + 2) + ": " + chars );
-    stringChars += (chars.length +2);
+    stringChars += (chars.length + 2);
   });
   $('#answer2 span').text( stringChars + " - " + codeChars + " = " + (stringChars - codeChars) );
-  //$('#bonus span').html(  );
 }
 
 $.get( "input.txt", function( data ) {
   $('#input span').text('(Bytes: ' + (data.length) + ')');
   part1( data );
   part2( data );
-  
-//   let input = [];
-//   data.forEach(function(number, index, array) {
-//     input.push(parseInt(number))
-//   });
-//   part1( input );
-//   part2( input );
 });
