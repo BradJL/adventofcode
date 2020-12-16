d5efea6ac8a68173d735b051b42ff3a75bdf41c0
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-16 06:48:01 -0500

    Update solution.js

--- 2020/day16/solution.js
+++ 2020/day16/solution.js
@@ -1,3 +1,5 @@
+// const fs = require('fs').promises;
+
 function initialize(){
   $('#answer span').text('Calculating...');
   $('#answer2 span').text('Calculating...');
@@ -8,34 +10,80 @@ function initialize(){
   // var ctx2 = canvas2.getContext("2d");  
 }
 
-// const fs = require('fs').promises;
-// async function readFile(filePath){
-//   try {
-//     const constData = await fs.readFile(filePath);
-//     let part1Answer = part1( constData, 'a' );
-//     console.log( "Part 1 answer: " + part1Answer );
-
-//     let part2Answer = part2( constData, 'a', part1Answer );
-//     console.log( "Part 2 answer: " + part2Answer );
-//   } catch (error) {
-//     console.error(`Got an error trying to read the file: ${error.message}`);
-//   }
-// }
-
 function log( what ){
   //console.log( what );
 }
   
 function part1( data, wire ){
-  data = data.toString().trim().split(/\r?\n/);
-  return 0;
+  data = data.trim().split(/\r?\n\r?\n/);
+  let validRanges = data[0].match(/[0-9]+-[0-9]+/g);
+  let validNumbers = [];//.fill(false, 0, 1000);
+  // let numbers = data[2].match(/[0-9]+/g);
+  let tickets = data[2].replace(/nearby tickets:\r?\n/,'').split(/\r?\n/);
+  console.log( "Total tickets: " + tickets.length );
+  let validTickets = [];
+  // log( "validRanges: " + validRanges );
+  // log( "numbers: " + numbers );
+
+  validRanges.forEach(function(value,index,array){
+    let nums = value.split(/-/);
+    for( i = parseInt( nums[0] ); i <= parseInt( nums[1] ); ++i ){
+      validNumbers[i] = true;
+    }
+  });
+  // console.log( "validNumbers: " + validNumbers );
+
+  // let invalidNumbers = new Set();
+  let invalidSum = 0;
+  // numbers.forEach(function(value,index,array){
+  tickets.forEach(function(value,index,array){
+    // console.log( "ticket: " + value );
+    let values = value.split(/,/);
+    let valid = true;
+    values.forEach(function(number,index,array){
+      let val = parseInt( number );
+      // console.log("val: " + val);
+      if( !validNumbers[val] ){
+          invalidSum += val;
+          valid = false;
+          // console.log( val + " is invalid")
+        } else {
+        // log( val + " is valid")
+      }
+    });
+    if( valid == true ){
+      validTickets.push( value );
+      // console.log( "Valid ticket: " + value );
+    }
+    // if( val < 28 || val > 971 ){
+    //   invalidSum += val;
+    //   console.log( val );
+    // }
+  });
+  console.log( "Valid tickets: " + validTickets.length );
+  return [invalidSum, validTickets]; // sum of invalid, array of valid tix, your ticket string
 }
 
-function part2( data, wire, part1Answer ){
-  data = data.toString().trim().split(/\r?\n/);
-
-  return 0;
+function part2( data, validTickets ){
+  console.log( "validTickets: " );
+  validTickets.forEach(function(ticket,index,array){
+    console.log(ticket);
+  })
+  return 2843534243843;
 }
+
+// async function readFile(filePath){
+//   try {
+//     const constData = await fs.readFile(filePath);
+//     let part1Answer = part1( constData.toString() );
+//     console.log( "Part 1 answer: " + part1Answer[0] );
+
+//     let part2Answer = part2( constData.toString(), part1Answer[1] );
+//     console.log( "Part 2 answer: " + part2Answer );
+//   } catch (error) {
+//     console.error(`Got an error trying to read the file: ${error.message}`);
+//   }
+// }
 
 function readFile(filePath){
   $.get( filePath, function( data ) {
