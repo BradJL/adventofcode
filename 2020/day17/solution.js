841ab259d831d748caaebf0c4758e6b7a51e3bfa
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-17 05:22:40 -0500

    Update solution.js

--- 2020/day17/solution.js
+++ 2020/day17/solution.js
@@ -53,11 +53,11 @@ function visualize( objects ){
 //     box.material = greenMat;
 //   });
   
-  let box = createBox( 1, 1, 1, 1.0, scene );
-  box.material = greenMat;
-  box = createBox( 2, 2, 2, 1.0, scene );
-  box = createBox( -1, -1, -1, 1.0, scene );
-  box.material = greenMat;
+  let box2 = createBox( 1, 1, 1, 1.0, scene );
+  box2.material = greenMat;
+  box2 = createBox( 2, 2, 2, 1.0, scene );
+  box2 = createBox( -1, -1, -1, 1.0, scene );
+  box2.material = greenMat;
   
   engine.runRenderLoop(function () {
     scene.render();
