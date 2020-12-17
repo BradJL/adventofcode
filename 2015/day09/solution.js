5c74baa09cb7e5531d5425880f94be0621b5d99e
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-16 22:33:03 -0500

    Update solution.js

--- 2015/day09/solution.js
+++ 2015/day09/solution.js
@@ -35,7 +35,6 @@ function getPlaces( data ){
 function initializeDistances( length ){
   let distances = new Array( length );
   for( let i = 0; i < length; ++i ){
-    // console.log( new Array( length ).fill( 0, 0, length ) );
     distances[i] = new Array( length ).fill( 0, 0, length );
   }
   return distances;
@@ -43,10 +42,9 @@ function initializeDistances( length ){
   
 function part1( data, wire ){
   let places = getPlaces(data);
-  console.log( places );
+  log( places );
 
   let distances = initializeDistances( Object.keys(places).length );
-//   console.log( distances );
 
   data = data.trim().split(/\r?\n/);
   data.forEach(function(line,index,array){
@@ -57,43 +55,41 @@ function part1( data, wire ){
     distances[ places[twoPlaces[1]] ][ places[twoPlaces[0]] ] = distance;
   });
 
-  console.log( distances );
+  log( distances );
   let path = {used:[], available:[0,1,2,3,4,5,6,7], sum:0};
   let paths = [path];
 
-  console.log( "paths.length: " + paths.length );
+  log( "paths.length: " + paths.length );
 
   let sumsSet = new Set();
   let min = 9999999;
   //let iterations = 8
   while( paths.length /*&& iterations--*/ ){
     path = paths.shift();
-    //console.log( path );
     if( path.available.length == 0 ){
       if( path.sum < min ){
         min = path.sum;
-        console.log( path );
       }
       sumsSet.add( path.sum );
     } else {
       path.available.forEach(function(avail,index,array){
         let newPath = clone( path );
         if( newPath.used.length ){
-        //   console.log( "sum was: " + newPath.sum );
-        //   console.log( "indicies: " + (newPath.used[newPath.used.length-1]) + "," + avail );
-        //   console.log( "distance: " + distances[newPath.used[newPath.used.length-1]][avail] );
+        //   log( "sum was: " + newPath.sum );
+        //   log( "indicies: " + (newPath.used[newPath.used.length-1]) + "," + avail );
+        //   log( "distance: " + distances[newPath.used[newPath.used.length-1]][avail] );
           newPath.sum += distances[newPath.used[newPath.used.length-1]][avail];
-        //   console.log( "sum is: " + newPath.sum );
+        //   log( "sum is: " + newPath.sum );
         }
         newPath.used.push( newPath.available.splice(index, 1)[0] );
         paths.push( newPath );
       });
-      //console.log( paths );
+      //log( paths );
     }
   }
 
   sumsSet = (Array.from(sumsSet)).sort(function(a, b){return a-b});
-  console.log( sumsSet );
+  log( sumsSet );
 
   return [ sumsSet[0]. sumsSet[ sumsSet.length - 1 ] ];
 }
