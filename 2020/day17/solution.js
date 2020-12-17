f5fc6c035337149674a1e38e70d1bf41877069e5
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-17 04:07:52 -0500

    Update solution.js

--- 2020/day17/solution.js
+++ 2020/day17/solution.js
@@ -26,8 +26,8 @@ function visualize( objects ){
   let redMat = new BABYLON.StandardMaterial("redMat", scene);
   redMat.backFaceCulling = true;
   redMat.emissiveColor = new BABYLON.Color3(0.6, 0.0, 0.0);
-  redMat.specularColor = new BABYLON.Color3(0.1, 0.0, 0.0);
-  redMat.ambientColor = new BABYLON.Color3(0.1, 0.0, 0.0);
+  redMat.specularColor = new BABYLON.Color3(0.0, 0.0, 0.0);
+  redMat.ambientColor = new BABYLON.Color3(0.0, 0.0, 0.0);
   objects.forEach( function(value,index,array){
     box = createBox( (value[0]-10)/15, (value[1]-10)/15, value[2]/15, scene );
     box.material = redMat;
