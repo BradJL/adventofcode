eb0ec23603de9562df5713d8e17d159e71560e0a
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-17 04:06:56 -0500

    Update solution.js

--- 2020/day17/solution.js
+++ 2020/day17/solution.js
@@ -7,7 +7,7 @@ var createBox = function( x, y, z, scene ){
     
 const createScene =  (engine, canvas) => {
   const scene = new BABYLON.Scene(engine);
-  scene.clearColor = new BABYLON.Color3(0.0, 0.8, 0.0);
+  scene.clearColor = new BABYLON.Color3(0.0, 0.7, 0.0);
   const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
   camera.attachControl(canvas, true);
   const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
