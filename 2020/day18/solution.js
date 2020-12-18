7f74603aa49c764344526ca133f0f2016ebd48b5
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-18 01:32:00 -0500

    Update solution.js

--- 2020/day18/solution.js
+++ 2020/day18/solution.js
@@ -1,112 +1,76 @@
 function initialize(){
   $('#answer span').text('Calculating...');
   $('#answer2 span').text('Calculating...');
-  
-  // var canvas = document.getElementById("visualization");
-  // var ctx = canvas.getContext("2d");
-  // var canvas2 = document.getElementById("visualization2");
-  // var ctx2 = canvas2.getContext("2d");  
 }
 
 // const fs = require('fs').promises;
 
 function evaluate( line, part ){
-//   console.log( "Eval " + line );
   let parenLine = line.match(/\([^()]+\)/)[0];
-  //let iterations = 2;
   while( parenLine.length != line.length ){//&& iterations--){
-//     console.log( "Extraced " + parenLine );
     line = line.replace( parenLine, evaluate( parenLine, part ) );
-//     console.log( "Updated line to " + line );
     parenLine = line.match(/\([^()]+\)/)[0];
   }
-//   console.log( "Line now is just one parenthetical: " + line );
   line = line.replace(/^\(/,'').replace(/\)$/,'');
-//   console.log( "Removed outside parens " + line );
   let plusOrMult;
   if( part == 1 ){
     plusOrMult = line.match(/[0-9]+ [+\*] [0-9]+/);
-    //iterations = 2;
     while( plusOrMult ){//&& iterations--){
       plusOrMult = plusOrMult[0];
-  //     console.log( "Grabbed " + plusOrMult );
       let lOpR = plusOrMult.split(/ /g);
       switch( lOpR[1] ){
       case '+':
         line = line.replace( plusOrMult, ""+(parseInt(lOpR[0]) + parseInt(lOpR[2])) );
-  //       console.log( "Updated for Addition " + line );
         break;
       case '*':
         line = line.replace( plusOrMult, ""+(parseInt(lOpR[0]) * parseInt(lOpR[2])) );
-  //       console.log( "Updated for Multiplication " + line );
         break;
       default:
-  //       console.log( "Uh-oh, default hit: " + lOpR );
+        console.log( "Uh-oh, default hit: " + lOpR );
         break;
       }
       plusOrMult = line.match(/[0-9]+ [+\*] [0-9]+/);
     }
   } else if( part == 2 ){
     plusOrMult = line.match(/[0-9]+ [\+] [0-9]+/);
-    //iterations = 2;
     while( plusOrMult ){//&& iterations--){
       plusOrMult = plusOrMult[0];
-  //     console.log( "Grabbed " + plusOrMult );
       let lOpR = plusOrMult.split(/ /g);
       line = line.replace( plusOrMult, ""+(parseInt(lOpR[0]) + parseInt(lOpR[2])) );
-  //       line = line.replace( plusOrMult, ""+(parseInt(lOpR[0]) * parseInt(lOpR[2])) );
       plusOrMult = line.match(/[0-9]+ [\+] [0-9]+/);
     }
     plusOrMult = line.match(/[0-9]+ [\*] [0-9]+/);
-    //iterations = 2;
     while( plusOrMult ){//&& iterations--){
       plusOrMult = plusOrMult[0];
-  //     console.log( "Grabbed " + plusOrMult );
       let lOpR = plusOrMult.split(/ /g);
       line = line.replace( plusOrMult, ""+(parseInt(lOpR[0]) * parseInt(lOpR[2])) );
       plusOrMult = line.match(/[0-9]+ [\*] [0-9]+/);
     }
   }
-//   console.log( "End of line: " + line );
-  //line.split(' ').forEach(function(char,index,array)
   return parseInt( line );
 }
   
 function evaluate2( line ){
-//   console.log( "Eval " + line );
   let parenLine = line.match(/\([^()]+\)/)[0];
-  //let iterations = 2;
   while( parenLine.length != line.length ){//&& iterations--){
-//     console.log( "Extraced " + parenLine );
     line = line.replace( parenLine, evaluate2( parenLine ) );
-//     console.log( "Updated line to " + line );
     parenLine = line.match(/\([^()]+\)/)[0];
   }
-//   console.log( "Line now is just one parenthetical: " + line );
   line = line.replace(/^\(/,'').replace(/\)$/,'');
-//   console.log( "Removed outside parens " + line );
-//   let plusOrMult = line.match(/[0-9]+ [+\*] [0-9]+/);
   let plusOrMult = line.match(/[0-9]+ [\+] [0-9]+/);
-  //iterations = 2;
   while( plusOrMult ){//&& iterations--){
     plusOrMult = plusOrMult[0];
-//     console.log( "Grabbed " + plusOrMult );
     let lOpR = plusOrMult.split(/ /g);
     line = line.replace( plusOrMult, ""+(parseInt(lOpR[0]) + parseInt(lOpR[2])) );
-//       line = line.replace( plusOrMult, ""+(parseInt(lOpR[0]) * parseInt(lOpR[2])) );
     plusOrMult = line.match(/[0-9]+ [\+] [0-9]+/);
   }
   plusOrMult = line.match(/[0-9]+ [\*] [0-9]+/);
-  //iterations = 2;
   while( plusOrMult ){//&& iterations--){
     plusOrMult = plusOrMult[0];
-//     console.log( "Grabbed " + plusOrMult );
     let lOpR = plusOrMult.split(/ /g);
     line = line.replace( plusOrMult, ""+(parseInt(lOpR[0]) * parseInt(lOpR[2])) );
     plusOrMult = line.match(/[0-9]+ [\*] [0-9]+/);
   }
-//   console.log( "End of line: " + line );
-  //line.split(' ').forEach(function(char,index,array)
   return parseInt( line );
 }
 
@@ -115,9 +79,7 @@ function part1( data ){
   let sum = 0;
   data.forEach(function(line,index,array){
     line = "(" + line + ")";
-//     console.log( "Changed to " + line );
     sum += evaluate( line, 1 );
-//     console.log( "SUM is now " + sum )
   });
   
   return sum;
@@ -128,9 +90,7 @@ function part2( data ){
   let sum = 0;
   data.forEach(function(line,index,array){
     line = "(" + line + ")";
-//     console.log( "Changed to " + line );
     sum += evaluate( line, 2 );
-//     console.log( "SUM is now " + sum )
   });
   
   return sum;
@@ -138,12 +98,6 @@ function part2( data ){
 
 function readFile(filePath){
   $.get( filePath, function( data ) {
-//     data = "";
-//     data += "1 + (2 * 3) + (4 * (5 + 6))"; // 51
-//     data += '\n';
-//     data += "2 * 3 + (4 * 5)"; // 46
-//     data += '\n';
-//     data += "5 + (8 * 3 + 9 + 3 * 4 * 3)"; // 1445
     $('#input span').text('(Bytes: ' + (data.length) + ')');
     let part1Answer = part1( data );
     $('#answer span').text( part1Answer );
