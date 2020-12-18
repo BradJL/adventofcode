9a353ef3ae936b73b687feea21b5e2c5ba8a0ebc
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-18 00:37:32 -0500

    Update solution.js

--- 2020/day18/solution.js
+++ 2020/day18/solution.js
@@ -13,10 +13,53 @@ function initialize(){
 function log( what ){
   //console.log( what );
 }
+
+function evaluate( line ){
+  console.log( "Eval " + line );
+  line = "(" + line + ")";
+  console.log( "Changed to " + line );
+  let parenLine = line.match(/\([^()]+\)/)[0];
+  while( parenLine.length != line.length ){
+    console.log( "Extraced " + parenLine );
+    line = line.replace( parenLine, evaluate( parenLine ) );
+    console.log( "Updated line to " + line );
+    parenLine = line.match(/\([^()]+\)/)[0];
+  }
+  console.log( "Line now is just one parenthetical: " + line );
+  line = line.replace(/^\(/,'').replace(/\)$/,'');
+  console.log( "Removed outside parens " + line );
+  let plusOrMult = line.match(/[0-9]+ [+\*] [0-9]+/)[0];
+  while( plusOrMult ){
+    console.log( "Grabbed " + plusOrMult );
+    let lOpR = plusOrMult.split(/ /g);
+    switch( lOpR[1] ){
+    case '+':
+      line = line.replace( plusOrMult, ""+(parseInt(lOpR) + parseInt(lOpR)) );
+      console.log( "Updated for Addition " + line );
+      break;
+    case '*':
+      line = line.replace( plusOrMult, ""+(parseInt(lOpR) * parseInt(lOpR)) );
+      console.log( "Updated for Multiplication " + line );
+      break;
+    default:
+      console.log( "Uh-oh, default hit: " + lOpR );
+      break;
+    }
+    plusOrMult = line.match(/[0-9]+ [+\*] [0-9]+/)[0];
+  }
+  console.log( "End of line: " + line );
+  //line.split(' ').forEach(function(char,index,array)
+  return parseInt( line );
+}
   
 function part1( data ){
   data = data.trim().split(/\r?\n/);
-  return 0;
+  let sum = 0;
+  data.forEach(function(line,index,array){
+    sum += evaluate( line );
+  });
+  
+  return sum;
 }
 
 function part2( data ){
@@ -27,6 +70,7 @@ function part2( data ){
 
 function readFile(filePath){
   $.get( filePath, function( data ) {
+    data = "2 * 3 + (4 * 5)";
     $('#input span').text('(Bytes: ' + (data.length) + ')');
     let part1Answer = part1( data );
     $('#answer span').text( part1Answer );
