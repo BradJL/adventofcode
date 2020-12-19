c222797204a2c15f6e5fdc6bef33c4e6a3895846
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-19 00:31:29 -0500

    Update solution.js

--- 2020/day19/solution.js
+++ 2020/day19/solution.js
@@ -33,7 +33,7 @@ function part1( data ){
   let numberStr = rule.match(/[0-9]+/);
   while( numberStr && iterations--){
     numberStr = rule.match(/[0-9]+/)[0];
-    rule.replace( numberStr, getRule(rules, parseInt(numberStr)) );
+    rule = rule.replace( numberStr, getRule(rules, parseInt(numberStr)) );
     numberStr = rule.match(/[0-9]+/);
   }
   
