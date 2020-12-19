ba57d5cb936153a0ee089654fa553774a750c0ae
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-19 00:06:05 -0500

    Update solution.js

--- 2020/day19/solution.js
+++ 2020/day19/solution.js
@@ -15,13 +15,15 @@ function log( what ){
 }
   
 function part1( data ){
-  data = data.trim().split(/\r?\n/);
+  data = data.trim().split(/\r?\n\r?\n/);
+  
   return 0;
 }
 
 function part2( data ){
   data = data.trim().split(/\r?\n/);
-
+  let rules = data[0].sort();
+  console.log( rules[0] );
   return 0;
 }
 
