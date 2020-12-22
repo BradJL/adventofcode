f60e654406d48dc25f45f96ad303a30884c7125a
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-22 00:31:41 -0500

    Update solution.js

--- 2020/day22/solution.js
+++ 2020/day22/solution.js
@@ -18,7 +18,7 @@ function part1( data ){
   let player1 = [];
   let player2 = [];
   let decks = data.trim().split(/\r?\n\r?\n/);
-  decks[0].slice(1).split(/\r?\n/).forEach(function(card,index,array){
+  decks[0].split(/\r?\n/).slice(1).forEach(function(card,index,array){
     player1.push( parseInt( card ) );
   });
   decks[1].slice(1).split(/\r?\n/).forEach(function(card,index,array){
