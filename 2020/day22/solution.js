872c958fc482f692c314d8655e7f3bc3baf53e24
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-22 00:24:45 -0500

    Update solution.js

--- 2020/day22/solution.js
+++ 2020/day22/solution.js
@@ -15,9 +15,9 @@ function log( what ){
 }
   
 function part1( data ){
-  let decks = data.trim().split(/\r?\n\r?\n/);
   let player1 = [];
   let player2 = [];
+  let decks = data.trim().split(/\r?\n\r?\n/);
   decks[0].slice(1).split(/\r?\n/).forEach(function(card,index,array)[
     player1.push( parseInt( card );
   });
@@ -59,4 +59,4 @@ function readFile(filePath){
 // }
 
 initialize();
-readFile('input.txt');
+readFile('input1.txt');
