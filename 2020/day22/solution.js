8e63504c63d38c91dd5e2aeef8ca0cb61c3b4ecc
BradJL <75324653+BradJL@users.noreply.github.com>
2020-12-22 01:48:42 -0500

    Update solution.js

--- 2020/day22/solution.js
+++ 2020/day22/solution.js
@@ -61,7 +61,11 @@ function recursiveCombat( player1, player2 ){
     console.log( "-- Round " + round + " --" );
     console.log( "Player 1's deck: " + player1 );
     console.log( "Player 2's deck: " + player2 );
-    if( configurations[ score(clone(player1), [] ) * 100000 + score[ [], clone(player2)) ] ){
+    if( configurations[
+                        score(
+                               clone(player1), []
+                             ) * 100000 + score( [], clone(player2) )
+                      ] ){
       console.log( "ending game early..." );
       return [[1],[]];
     } else {
