cd6e889ee15e5d01f745df9f58c32737af45e497
MS <mythshare@gmail.com>
2021-12-01 12:08:39 -0500

    Day01: initial setup.

---
+++ 2021/day01/solution.js
@@ -0,0 +1,89 @@
+$('#answer span').text('Calculating...');
+$('#answer2 span').text('Calculating...');
+
+// var canvas = document.getElementById("visualization");
+// var ctx = canvas.getContext("2d");
+// var canvas2 = document.getElementById("visualization2");
+// var ctx2 = canvas2.getContext("2d");
+
+function part1( data ){
+  data.forEach(function(value, index, array) {
+  });
+  //$('#answer span').text(  );
+  //$('#bonus span').html(  );
+}
+function part2( data ){
+  data.forEach(function(value, index, array) {
+  });
+  //$('#answer2 span').text(  );
+  //$('#bonus span').html(  );
+}
+
+$.get( "input.txt", function( data ) {
+//   $('#input span').text('(Bytes: ' + (data.length) + ')');
+  data = data.trim().split(/\r?\n/);
+  $('#input span').text('(Lines: ' + (data.length) + ')');
+//   part1( data );
+//   part2( data );
+  
+//   let input = [];
+//   data.forEach(function(number, index, array) {
+//     input.push(parseInt(number))
+//   });
+//   part1( input );
+//   part2( input );
+});
+
+// OR this:
+function initialize(){
+  $('#answer span').text('Calculating...');
+  $('#answer2 span').text('Calculating...');
+  
+  // var canvas = document.getElementById("visualization");
+  // var ctx = canvas.getContext("2d");
+  // var canvas2 = document.getElementById("visualization2");
+  // var ctx2 = canvas2.getContext("2d");  
+}
+
+// const fs = require('fs').promises;
+
+function log( what ){
+  //console.log( what );
+}
+  
+function part1( data ){
+  data = data.trim().split(/\r?\n/);
+  
+  return 0;
+}
+
+function part2( data ){
+  data = data.trim().split(/\r?\n/);
+
+  return 0;
+}
+
+function readFile(filePath){
+  $.get( filePath, function( data ) {
+    $('#input span').text('(Bytes: ' + (data.length) + ')');
+    let part1Answer = part1( data );
+    $('#answer span').text( part1Answer );
+    let part2Answer = part2( data );
+    $('#answer2 span').text( part2Answer );
+  });
+}
+// async function readFile(filePath){
+//   try {
+//     const constData = await fs.readFile(filePath);
+//     let part1Answer = part1( constData.toString() );
+//     console.log( "Part 1 answer: " + part1Answer );
+
+//     let part2Answer = part2( constData.toString() );
+//     console.log( "Part 2 answer: " + part2Answer );
+//   } catch (error) {
+//     console.error(`Got an error trying to read the file: ${error.message}`);
+//   }
+// }
+
+initialize();
+readFile('input.txt');
