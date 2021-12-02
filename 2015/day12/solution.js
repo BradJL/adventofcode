2ef2e9078637fb1ef0f0423bc06aec09ab2b9383
MS <mythshare@gmail.com>
2021-12-02 01:52:21 -0500

    2015 day12: first puzzle attempt 1

---
+++ 2015/day12/solution.js
@@ -0,0 +1,107 @@
+$('#answer span').text('Calculating...');
+$('#answer2 span').text('Calculating...');
+
+// var canvas = document.getElementById("visualization");
+// var ctx = canvas.getContext("2d");
+// var canvas2 = document.getElementById("visualization2");
+// var ctx2 = canvas2.getContext("2d");
+
+function part1( data ){
+  data.forEach(function(value, index, array) {
+  });
+  //$('#answer span').text(  );
+  //$('#bonus span').html(  );
+}
+function part2( data ){
+  data.forEach(function(value, index, array) {
+  });
+  //$('#answer2 span').text(  );
+  //$('#bonus span').html(  );
+}
+
+$.get( "input.txt", function( data ) {
+//   $('#input span').text('(Bytes: ' + (data.length) + ')');
+  data = data.trim().split(/\r?\n/);
+  $('#input span').text('(Lines: ' + (data.length) + ')');
+//   part1( data );
+//   part2( data );
+  
+//   let input = [];
+//   data.forEach(function(number, index, array) {
+//     input.push(parseInt(number))
+//   });
+//   part1( input );
+//   part2( input );
+});
+
+// OR this:
+function initialize(){
+  $('#answer span').text('Calculating...');
+  $('#answer2 span').text('Calculating...');
+  
+  // var canvas = document.getElementById("visualization");
+  // var ctx = canvas.getContext("2d");
+  // var canvas2 = document.getElementById("visualization2");
+  // var ctx2 = canvas2.getContext("2d");  
+}
+
+// const fs = require('fs').promises;
+
+function log( what ){
+  //console.log( what );
+}
+  
+function part1( data ){
+	let sum = 0;
+	const regex = /[^0-9-,]/i;
+	data.replace(regex, '');
+	data = data.trim().split(/,/).forEach(function(valStr,index,array){
+		sum += parseInt(valStr);
+	});
+
+	return sum;
+}
+
+function part2( data ){
+	let forward = 0;
+	let aim = 0;
+	let depth = 0;
+	data = data.trim().split(/\r?\n/).forEach(function(valStr,index,array){
+		let vals = valStr.split(/ /);
+		if( vals[0] == "forward" ){
+			forward += parseInt(vals[1]);
+			depth += aim*parseInt(vals[1]);
+		} else if( vals[0] == "down" ){
+			aim += parseInt(vals[1]);
+		} else {
+			aim -= parseInt(vals[1]);
+		}
+	});
+
+	return forward*depth;
+}
+
+function readFile(filePath){
+  $.get( filePath, function( data ) {
+    $('#input span').text('(Bytes: ' + (data.length) + ')');
+    let part1Answer = part1( data );
+    $('#answer span').text( part1Answer );
+    let part2Answer = part2( data );
+    $('#answer2 span').text( part2Answer );
+  });
+}
+// async function readFile(filePath){
+//   try {
+//     const constData = await fs.readFile(filePath);
+//     let part1Answer = part1( constData.toString() );
+//     console.log( "Part 1 answer: " + part1Answer );
+
+//     let part2Answer = part2( constData.toString() );
+//     console.log( "Part 2 answer: " + part2Answer );
+//   } catch (error) {
+//     console.error(`Got an error trying to read the file: ${error.message}`);
+//   }
+// }
+
+initialize();
+readFile('input.txt');
