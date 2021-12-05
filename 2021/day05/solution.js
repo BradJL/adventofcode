5d1d0c965486509688a551907dc2dfd5d6b32b6e
MS <mythshare@gmail.com>
2021-12-05 18:09:47 -0500

    2021 day05: cleanup.

--- 2021/day05/solution.js
+++ 2021/day05/solution.js
@@ -1,8 +1,8 @@
 $('#answer span').text('Calculating...');
 $('#answer2 span').text('Calculating...');
 
-// var canvas = document.getElementById("visualization");
-// var ctx = canvas.getContext("2d");
+var canvas = document.getElementById("visualization");
+var ctx = canvas.getContext("2d");
 var canvas2 = document.getElementById("visualization2");
 var ctx2 = canvas2.getContext("2d");
 
@@ -44,7 +44,6 @@ const ARRAY_SIZE = 1000;
 function part1( data ){
 	let ocean_floor_vents = new Array(ARRAY_SIZE*ARRAY_SIZE).fill(0);
 	let twos = 0;
-	//let bingo_cards = {};
 
 	data.trim().split(/\r?\n/).forEach(function(valStr,index,array){
 		let vals = new Array();
@@ -90,6 +89,10 @@ function part1( data ){
 }
 
 function part2( data ){
+	calculate_vents( data, true );
+}
+
+function calculate_vents( data, allow_diagonals ){
 	let ocean_floor_vents = new Array(ARRAY_SIZE*ARRAY_SIZE).fill(0);
 	let twos = 0;
 
@@ -99,77 +102,39 @@ function part2( data ){
 			vals.push( parseInt(valStr1) );
 		});
 
-		// vals: [[ x1,y1,x2,y2 ], ...]
-		//console.log( vals );
-//		if( vals[ 0 ] == vals[ 2 ] ){
-//			// horizontal line
-//			let from = vals[ 1 ];
-//			let thru = vals[ 3 ];
-//			if( from > thru ){
-//				from = thru;
-//				thru = vals[ 1 ];
-//			}
-//			while( from <= thru ){
-//				ocean_floor_vents[ vals[0] + ARRAY_SIZE*from ] += 1;
-//				//console.log( ocean_floor_vents[ vals[0] + ARRAY_SIZE*from ] )
-//				if( ocean_floor_vents[ vals[0] + ARRAY_SIZE*from ] == 2 ){ ++twos }
-//				++from;
-//			}
-//		} else if( vals[ 1 ] == vals[ 3 ] ){
-//			// vertical line
-//			let from = vals[ 0 ];
-//			let thru = vals[ 2 ];
-//			if( from > thru ){
-//				from = thru;
-//				thru = vals[ 0 ];
-//			}
-//			while( from <= thru ){
-//				ocean_floor_vents[ from + ARRAY_SIZE*vals[1] ] += 1;
-//				//console.log( ocean_floor_vents[ from + ARRAY_SIZE*vals[0] ] )
-//				if( ocean_floor_vents[ from + ARRAY_SIZE*vals[1] ] == 2 ){ ++twos }
-//				++from;
-//			}
-//		} else {
-			// diagonal line
-			let fromy = vals[ 0 ];
-			let thruy = vals[ 2 ];
-			let fromx = vals[ 1 ];
-			let thrux = vals[ 3 ];
-//			if( fromy > thruy ){
-//				fromy = thruy;
-//				thruy = vals[ 0 ];
-//				fromx = vals[ 3 ];
-//				thrux = vals[ 1 ];
-//			}
+		let fromy = vals[ 0 ];
+		let thruy = vals[ 2 ];
+		let fromx = vals[ 1 ];
+		let thrux = vals[ 3 ];
 
-ctx2.strokeStyle = "#FF0000";
-ctx2.beginPath();
-ctx2.moveTo(fromx, fromy);
-ctx2.lineTo(thrux, thruy);
-ctx2.stroke();
+		let ydelta = 1;
+		if( fromy > thruy){
+			ydelta = -1;
+		} else if( fromy == thruy ){
+			ydelta = 0;
+		}
 
-			let ydelta = 1;
-			if( fromy > thruy){
-				ydelta = -1;
-			} else if( fromy == thruy ){
-				ydelta = 0;
-			}
+		let xdelta = 1;
+		if( fromx > thrux ){
+			xdelta = -1;
+		} else if( fromx == thrux ){
+			xdelta = 0;
+		}
 
-			let xdelta = 1;
-			if( fromx > thrux ){
-				xdelta = -1;
-			} else if( fromx == thrux ){
-				xdelta = 0;
-			}
-
+		if( allow_diagonals || (!(xdelta && ydelta)) ){
+			ctx2.strokeStyle = "#FF0000";
+			ctx2.beginPath();
+			ctx2.moveTo(fromx, fromy);
+			ctx2.lineTo(thrux, thruy);
+			ctx2.stroke();
+		
 			while( ( fromy != thruy + ydelta ) || ( fromx != thrux + xdelta ) ){
 				ocean_floor_vents[ fromy + ARRAY_SIZE*fromx ] += 1;
 				if( ocean_floor_vents[ fromy + ARRAY_SIZE*fromx ] == 2 ){ ++twos }
 				fromy += ydelta;
 				fromx += xdelta;
 			}
-//		}
-
+		}
 		// Debug output
 //		let str = "";
 //		ocean_floor_vents.forEach(function(val,index,array){
